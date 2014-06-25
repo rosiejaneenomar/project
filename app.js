f0c16d4eaf3abb17bfc76f69256f3d595f0d78d5
iloverein <rjxirc@gmail.com>
2014-06-25 09:01:52 +0800

    Modified json file

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
+t var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+alert("STOP!!");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
